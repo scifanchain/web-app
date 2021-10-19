9b56da57675c7c2a6658bab34907c0c52a74cc63
unity <unityoxb@gmail.com>
2021-10-19 21:41:57 +0800

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Menu, Divider } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     const { activeItem, setActiveItem } = useState('closest')
@@ -7,24 +8,26 @@ export default function Footer() {
     const handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
     return (
-        <Container fluid style={{backgroundColor: '#000'}}>
+        <Container fluid style={{ backgroundColor: '#000' }}>
             <Menu fluid text inverted>
                 <Menu.Item>&copy; 赛凡网络 2021</Menu.Item>
                 <Menu.Item
-                    name='closest'
-                    active={activeItem === 'closest'}
+                    as={Link}
+                    to={{
+                        pathname = 'https://whitebook.scifanchain.com'
+                    }}
+                    name='whitebook'
+                    active={activeItem === 'whitebook'}
                     content="白皮书"
                     onClick={handleItemClick}
                 />
-                {/* <Menu.Item
-                    name='mostComments'
-                    active={activeItem === 'mostComments'}
-                    content="用户手册"
-                    onClick={handleItemClick}
-                /> */}
                 <Menu.Item
-                    name='mostPopular'
-                    active={activeItem === 'mostPopular'}
+                    as={Link}
+                    to={{
+                        pathname = 'https://whitebook.scifanchain.com'
+                    }}
+                    name='docs'
+                    active={activeItem === 'docs'}
                     content="文档"
                     onClick={handleItemClick}
                 />
